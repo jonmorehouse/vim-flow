b4bf57cd570cc4b3b1d10a616657c2bbb5f49562
Jon Morehouse <morehousej09@gmail.com>
2014-06-18 21:04:19 -0700

    Fix ruby runner, add in ability to call individual files

--- flows/ruby.py
+++ flows/ruby.py
@@ -5,13 +5,19 @@ import re
 filenames = ["Rakefile"]
 extensions = ["rb"]
 
+
 def run(**kw):
 
+    if u.has_file(kw, "Rakefile"):
+        command = "bundle exec rake spec"
+    else:
+        command = "ruby %s" % kw.get("filepath")
+
     # run tests by default ... this shouldn't be that slow (hopefully)
-    u.shell("rake spec", **kw)
+    u.shell(command, **kw)
     
 def test(**kw):
 
     # run the applicatin in the simulator
-    u.tmux_shell("rake")
+    u.tmux_shell("bundle exec rake")
 
