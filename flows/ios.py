79be48d6e49bab7563d6670bc18f4edba0f9b42c
Jon Morehouse <morehousej09@gmail.com>
2014-10-15 17:27:30 -0700

    Fix ios flow so that it cancels out processes properly

--- flows/ios.py
+++ flows/ios.py
@@ -4,7 +4,10 @@ extensions = ["h", "m"]
 
 def run(**kw):
 
-    if u.execute_project_file(**kw):
+    project_file = u.get_file(kw, ".project")
+    if project_file:
+        u.python_shell("tmux split-window -t temp")
+        u.tmux_shell("tmux kill-pane -t 0 && %s " % project_file, session = "temp", pane = 1)
         return
     
     command = u.basepath_command("xctool build", **kw)
