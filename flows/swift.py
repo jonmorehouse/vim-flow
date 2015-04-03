bd8c57110b893239111bd59faa92b647e5483776
Jon Morehouse <morehousej09@gmail.com>
2015-04-03 11:29:43 -0700

    Add in swift flow

--- flows/swift.py
+++ flows/swift.py
@@ -2,17 +2,10 @@ import flowtils as u
 import vim
 import re
 
-filenames = ["Rakefile"]
-extensions = ["rb"]
+extensions = ["swift"]
 
 def run(**kw):
     project_file_contents = u.get_file(kw, ".project")
     if project_file_contents:
         u.tmux_shell(project_file_contents, session = "temp", pane = 0)
         return
-
-    command = "ruby %s" % kw.get("filepath")
-    u.shell(command, **kw)
-
-
-
